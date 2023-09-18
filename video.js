f9c0979d0c636e2fe4195bb61c2dded26f4a9e4b
timothelaborie <tlaborie@student.ethz.ch>
2023-09-18 15:47:22 +0200

    average

--- video.js
+++ video.js
@@ -25,25 +25,76 @@ async function onload() {
             console.log(rate);
             paella.player.videoContainer.setPlaybackRate(rate);
         }
-        // else if (e.keyCode == '37') {
-        //     // left arrow
-        //     paella.player.videoContainer.currentTime()
-        //         .then(function (currentTime) {
-        //             paella.player.videoContainer.seekToTime(currentTime + 0);
-        //         });
-        // }
-        // else if (e.keyCode == '39') {
-        //     // right arrow
-        //     paella.player.videoContainer.currentTime()
-        //         .then(function (currentTime) {
-        //             paella.player.videoContainer.seekToTime(currentTime - 0);
-        //         });
-        // }
+        else if (e.keyCode == '37') {
+            // left arrow
+            paella.player.videoContainer.currentTime()
+                .then(function (currentTime) {
+                    paella.player.videoContainer.seekToTime(currentTime - 2);
+                });
+        }
+        else if (e.keyCode == '39') {
+            // right arrow
+            paella.player.videoContainer.currentTime()
+                .then(function (currentTime) {
+                    paella.player.videoContainer.seekToTime(currentTime + 2);
+                });
+        }
 
     }
 
+    let totalGrade = 0;
+    let totalWeight = 0;
+    let weightedTotal = 0;
+    let count = 0;
+    
+    $('table tr').each(function() {
+        let $firstTd = $(this).find('td:first');
+        
+        // Check for "Additional Requirements" and break the loop if found.
+        if ($firstTd.text().includes('Additional Requirements')) {
+            return false;  // This will break out of the .each loop.
+        }
+    
+        // Check if the first td contains " S".
+        if ($firstTd.text().endsWith(' S') &&  $(this).find('td').eq(4).text() == "") {
+            let courseName = $(this).find('td').eq(1).text().trim();
+            let grade = parseFloat($(this).find('td').eq(3).text().trim());
+            let weight = parseInt($(this).find('td').eq(5).text().trim(), 10);
+    
+            // Print the values to the console.
+            console.log(`Course: ${courseName}, Grade: ${grade}, Weight: ${weight}`);
+            
+            totalGrade += grade;
+            weightedTotal += grade * weight;
+            totalWeight += weight;
+            count++;
+        }
+        if ($firstTd.attr('colspan') == 2) {
+            let courseName = $firstTd.text().trim();
+            let grade = parseFloat($(this).find('td').eq(2).text().trim());
+            let weight = parseInt($(this).find('td').eq(4).text().trim(), 10);
+    
+            // Print the values to the console.
+            console.log(`Course: ${courseName}, Grade: ${grade}, Weight: ${weight}`);
+    
+            totalGrade += grade;
+            weightedTotal += grade * weight;
+            totalWeight += weight;
+            count++;
+        }
+        
+    });
+    
+    let average = totalGrade / count;
+    let weightedAverage = weightedTotal / totalWeight;
+    
+    console.log(`Average: ${average}`);
+    console.log(`Weighted Average: ${weightedAverage}`);
 
-    console.log('video.js done');
+    text = $("table.tablebox p").text();
+    $("table.tablebox p").html(text + "<br>" + `Average: ${average}` + "<br>" + `Weighted Average: ${weightedAverage}`);
+
+    // console.log('video.js done');
 
 
 }
